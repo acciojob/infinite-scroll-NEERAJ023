081e6ed09722e07992a66caf536c69d4a3733381
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-21 22:21:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 //your code here!
+document.addEventListener("DOMContentLoaded", function () {
+  const infiList = document.getElementById("infi-list");
 
+  // Function to add items to the list
+	let b=1;
+  function addItems(count) {
+    for (let i = 1; i <= count; i++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `Item ${b}`;
+		b++;
+      infiList.appendChild(listItem);
+    }
+  }
+
+  // Add 10 items by default
+  addItems(10);
+
+  // Event listener for scrolling
+  infiList.addEventListener("scroll", function () {
+    // Check if the user has reached the end of the list
+    if (
+      infiList.scrollTop + infiList.clientHeight >=
+      infiList.scrollHeight - 10
+    ) {
+      // Add 2 more items
+      addItems(2);
+    }
+  });
+});
